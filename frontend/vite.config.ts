fb63a03c8ff83894cefa6e16d29b2c4172b395dc
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-10-30 14:46:20 +0200

    fix:adjusted vite configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 import netlify from "@netlify/vite-plugin";
-
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-, tailwindcss(),
-netlify()
-  ],
-})
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), tailwindcss(), netlify()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/",
+});
